70f6ea7b8c5af92c63e50431af1f6f4a3ca41a6d
José Isaac <joseisaacnascimento@gmail.com>
2025-08-05 12:53:40 -0300

    feat: Cria componentes de Loader

---
+++ client/src/Components/Loader/index.jsx
@@ -0,0 +1,10 @@
+const Loader = () => {
+  return (
+    <div>
+      <div className="h-20 w-20 animate-spin rounded-full border-4 border-solid border-current border-r-transparent"></div>
+      <h1>Carregando...</h1>
+    </div>
+  );
+};
+
+export default Loader;
