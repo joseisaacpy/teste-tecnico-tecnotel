80ad06f231999a81815b87ef9dc9581a1aae6466
José Isaac <joseisaacnascimento@gmail.com>
2025-08-05 07:45:21 -0300

    feat: Cria estrutura inicial do frontend com React e Vite (setup básico, config e pasta src)

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+      {/* Header */}
+      <header className="bg-slate-900 p-2 flex justify-center items-center">
+        <h1 className="text-3xl text-white font-bold">Sistema de Tarefas</h1>
+      </header>
+      {/* Section Principal */}
+      <section></section>
+      {/* Footer */}
+      <footer className="bg-slate-900 p-2">
+        <p className="text-white text-center">
+          Desenvolvido por{" "}
+          <a href="https://github.com/joseisaacpy">José Isaac</a>
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
