b37f5423411fac908aa7d9a75be10311e4c16c2b
José Isaac <joseisaacnascimento@gmail.com>
2025-08-04 21:05:01 -0300

    feat: Implementa endpoint POST /tasks para criar uma tarefa

--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -7,6 +7,21 @@ const tasksRouter = Router();
 
 // Rotas
 // Cria uma nova tarefa
+tasksRouter.post("/", async (req, res) => {
+  try {
+    // Cria uma nova tarefa
+    const task = await prisma.task.create({
+      data: req.body,
+    });
+    // Envia a tarefa
+    res.json({
+      message: "Tarefa criada com sucesso.",
+      task,
+    });
+  } catch (error) {
+    res.status(500).send("Erro ao criar tarefa.");
+  }
+});
 
 // Pega todas as tarefas
 tasksRouter.get("/", async (req, res) => {
@@ -16,7 +31,7 @@ tasksRouter.get("/", async (req, res) => {
 
     // Valida se tem alguma tarefa
     if (tasks.length === 0) {
-      return res.status(404).send("Nenhuma tarefa encontrada.");
+      return res.status(404).json({ msg: "Nenhuma tarefa encontrada." });
     }
     // Envia as tarefas
     res.json(tasks);
